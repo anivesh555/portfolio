c0b6de7208b44cfb66fd3169dfdb73286c2fadff
Anivesh_0777 <v-anivesh.b@mindtickle.com>
2025-09-13 18:26:53 +0530

    my portfoliio

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,12 +12,12 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/abu-said-bd/" className="text-[#16f2b3]">Abu Said</Link>
+            © Developer Portfolio by <Link target="_blank" href="" className="text-[#16f2b3]"></Link>
           </p>
           <div className="flex items-center gap-5">
             <Link
               target="_blank"
-              href="https://github.com/said7388/developer-portfolio"
+              href=""
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <IoStar />
@@ -25,11 +25,11 @@ function Footer() {
             </Link>
             <Link
               target="_blank"
-              href="https://github.com/said7388/developer-portfolio/fork"
+              href=""
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <CgGitFork />
-              <span>Fork</span>
+              <span>later will add this </span>
             </Link>
           </div>
         </div>
--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'abhadauriya.8445@gmail.com',
+    phone: '+918475982009',
+    address: 'Delhi, India',
+    github: 'https://github.com/anivesh555',
+    facebook: 'https://www.facebook.com/',
+    linkedIn: 'https://www.linkedin.com/in/',
+    twitter: 'https://twitter.com/',
+    stackOverflow: 'https://stackoverflow.com/users/16840768',
+    devUsername: ""
 }
